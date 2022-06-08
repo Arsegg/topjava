4b4aeca38063b9c8c693e18f06d903910ec71e5e
Arsegg <arsegg@mail.ru>
2022-06-09 00:30:11 +0400

    HW01 Inlined meals

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import ru.javawebinar.topjava.model.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.RequestDispatcher;
@@ -12,24 +11,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
     private static final int CALORIES_PER_DAY = 2_000;
-    private static final List<MealTo> MEAL_TOS = MealsUtil.filteredByStreams(MealsUtil.MEALS,
-            LocalTime.MIN,
-            LocalTime.MAX,
-            CALORIES_PER_DAY);
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm");
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         log.debug("Setting attributes...");
         request.setAttribute("dateTimeFormatter", DATE_TIME_FORMATTER);
-        request.setAttribute("meals", MEAL_TOS);
+        request.setAttribute("meals", MealsUtil.filteredByStreams(MealsUtil.MEALS,
+                LocalTime.MIN,
+                LocalTime.MAX,
+                CALORIES_PER_DAY));
         log.debug("Attributes are set.");
 
         log.debug("Forwarding to meals...");
